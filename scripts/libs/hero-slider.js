58ca4a65549fefc2fd40eefd60c257acc5596a8e
morie0108 <65838657+morie0108@users.noreply.github.com>
2020-10-21 00:38:56 +0900

    Add files via upload

--- scripts/libs/hero-slider.js
+++ scripts/libs/hero-slider.js
@@ -1,8 +1,7 @@
 let swiper = new Swiper(".swiper-container", {
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  autoplay: {
+    delay: 3000,
   },
   pagination: {
     el: ".swiper-pagination",
