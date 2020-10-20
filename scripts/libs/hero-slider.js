4ca873943f488653fe17ffb55389b8bde86c81c4
morie0108 <65838657+morie0108@users.noreply.github.com>
2020-10-20 23:06:06 +0900

    Add files via upload

--- scripts/libs/hero-slider.js
+++ scripts/libs/hero-slider.js
@@ -1,11 +1,22 @@
 let swiper = new Swiper(".swiper-container", {
   loop: true,
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
+  },
+  slidesPerView: 1,
+  breakpoints: {
+    // 960px以上の場合
+    960: {
+      slidesPerView: 2,
+    },
+    // 1200px以上の場合
+    1200: {
+      slidesPerView: 4,
+    },
   },
 });
